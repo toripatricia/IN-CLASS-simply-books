d67b1863e5b64c10042d3200eb784682f0812f1a
Tori Franklin <toripfranklin@gmail.com>
2023-04-15 12:53:15 -0400

    setup

---
+++ pages/author/new.js
@@ -0,0 +1,3 @@
+export default function AddAuthor() {
+  return <div>Add Author</div>;
+}
--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Profile() {
-  return <div>profile here</div>;
+  return <div>Profile Here</div>;
 }
